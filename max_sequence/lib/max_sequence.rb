903b7bc73f0730227ede8ffe592314058bd02183
Natalia_Sherchenkova <tataterer@gmail.com>
2021-06-08 12:03:56 +0100

    first test with one positive number in the array passes

---
+++ max_sequence/lib/max_sequence.rb
@@ -0,0 +1,5 @@
+# max_sequence method returns the max sum of a consecutive sequence in the array
+
+def max_sequence(array)
+  array.first
+end
--- max_sequence/spec/max_sequence_spec.rb
+++ max_sequence/spec/max_sequence_spec.rb
@@ -1,1 +1,7 @@
-require max_sequence.rb
+require 'max_sequence'
+
+describe 'max_sequence()' do
+  it 'returns a positive number when only one number is given in array' do
+    expect(max_sequence([11])).to eq(11)
+  end
+end
