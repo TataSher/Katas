2d0a16be8020c42ac7cf1d26c04db93c5cbdf6d7
Natalia_Sherchenkova <tataterer@gmail.com>
2021-06-08 12:11:08 +0100

    method returns 0 if several negative numbers are given only

--- max_sequence/lib/max_sequence.rb
+++ max_sequence/lib/max_sequence.rb
@@ -1,5 +1,9 @@
 # max_sequence method returns the max sum of a consecutive sequence in the array
 
 def max_sequence(array)
-  array.sum
+  if array.sum < 0
+    return 0
+  else
+    array.sum
+  end
 end
--- max_sequence/spec/max_sequence_spec.rb
+++ max_sequence/spec/max_sequence_spec.rb
@@ -7,4 +7,10 @@ describe 'max_sequence()' do
   it 'returns a sum of positive numbers if several positive numbers are given' do
     expect(max_sequence([1, 2, 3])).to eq(6)
   end
+  it 'returns a 0 if only a negative number is given' do
+    expect(max_sequence([-1])).to eq(0)
+  end
+  it 'returns a 0 if several negative numbers are given only' do
+    expect(max_sequence([-1, -2, -3])).to eq(0)
+  end
 end
