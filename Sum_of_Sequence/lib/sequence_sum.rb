66d884b45cc5f371d37c82b5ac518fb3e6d7637d
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-05 16:23:19 +0100

    refactored with reduce method

--- Sum_of_Sequence/lib/sequence_sum.rb
+++ Sum_of_Sequence/lib/sequence_sum.rb
@@ -1,3 +1,3 @@
 def sequence_sum(start_value, end_value, step_value)
-  start_value > end_value ? 0 : (start_value..end_value).step(step_value).to_a.sum
+  start_value > end_value ? 0 : (start_value..end_value).step(step_value).reduce(:+)
 end
