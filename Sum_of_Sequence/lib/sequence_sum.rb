5e6b2f0e74a71285b2b9db269f1fb124127e1d02
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-05 13:21:42 +0100

    returns the sum of the sequence with odd step value

--- Sum_of_Sequence/lib/sequence_sum.rb
+++ Sum_of_Sequence/lib/sequence_sum.rb
@@ -1,9 +1,9 @@
 def sequence_sum(start_value, end_value, step_value)
   array = (start_value..end_value).to_a
-  @new_array = []
-  array.map do |number|
-    if number % step_value == 0
-      @new_array << number
+  @new_array = [start_value]
+  array.map do |value|
+    if @new_array.last < end_value
+      @new_array << @new_array.last + step_value
     end
   end
   start_value > end_value ? 0 : @new_array.sum
--- Sum_of_Sequence/spec/sequence_sum_spec.rb
+++ Sum_of_Sequence/spec/sequence_sum_spec.rb
@@ -4,10 +4,13 @@ describe '#sequence_sum' do
   it 'receives the same begin, end and step values and returns the original value' do
     expect(sequence_sum(2,2,2)).to eq(2)
   end
-  it 'receives begin, end and step values and returns the sum of the seqence created' do
+  it 'receives begin, end and step values and returns the sum of the sequence created' do
     expect(sequence_sum(2,6,2)).to eq(12)
   end
   it 'returns 0 if the begin value is greater than the end' do
     expect(sequence_sum(4,2,2)).to eq(0)
+  end
+  it 'receives begin, end and odd step value and returns the sum of the sequence created' do
+    expect(sequence_sum(1,5,1)).to eq(15)
   end
 end
