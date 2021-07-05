b027a6ee1fa1f1ec34b1dff80a99c0ba5408b211
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-05 13:02:00 +0100

    renamed the method to be sequence_sum

--- Sum_of_Sequence/lib/sequence_sum.rb
+++ Sum_of_Sequence/lib/sequence_sum.rb
@@ -1,4 +1,4 @@
-def sequenceSum(start_value, end_value, step_value)
+def sequence_sum(start_value, end_value, step_value)
   array = (start_value..end_value).to_a
   @new_array = []
   array.map do |number|
--- Sum_of_Sequence/spec/sequence_sum_spec.rb
+++ Sum_of_Sequence/spec/sequence_sum_spec.rb
@@ -1,13 +1,13 @@
 require 'sequence_sum'
 
-describe '#sequenceSum' do
+describe '#sequence_sum' do
   it 'receives the same begin, end and step values and returns the original value' do
-    expect(sequenceSum(2,2,2)).to eq(2)
+    expect(sequence_sum(2,2,2)).to eq(2)
   end
   it 'receives begin, end and step values and returns the sum of the seqence created' do
-    expect(sequenceSum(2,6,2)).to eq(12)
+    expect(sequence_sum(2,6,2)).to eq(12)
   end
   it 'returns 0 if the begin value is greater than the end' do
-    expect(sequenceSum(4,2,2)).to eq(0)
+    expect(sequence_sum(4,2,2)).to eq(0)
   end
 end
