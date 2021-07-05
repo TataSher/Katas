ffb0f292aa17fac95d24110ffa83898781887f6e
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-05 19:52:11 +0100

    set-up triangular treasure kata. first test passes

---
+++ Triangular_Treasure/lib/triangular.rb
@@ -0,0 +1,3 @@
+def triangular(number)
+  number
+end
---
+++ Triangular_Treasure/spec/triangular_spec.rb
@@ -0,0 +1,7 @@
+require 'triangular'
+
+describe 'triangular' do
+  it 'returns 1 when given 1' do
+    expect(triangular(1)).to eq 1
+  end
+end
