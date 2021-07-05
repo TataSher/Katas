1e023b49872cf133f43d67b4810ef2e956672e1b
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-05 19:07:21 +0100

    arithmetic function kata set-up

--- Sum_of_Sequence/lib/sequence_sum.rb
+++ Sum_of_Sequence/lib/sequence_sum.rb
@@ -1,3 +1,3 @@
 def sequence_sum(start_value, end_value, step_value)
- (start_value..end_value).step(step_value).each {|number| number}.sum || 0
+ (start_value..end_value).step(step_value).reduce(:+) || 0
 end
