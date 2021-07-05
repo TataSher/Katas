b9622ce9286ff308f3e031f82553ce6af09632fb
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-05 19:20:31 +0100

    test with subtract and multiply operator passes

--- Arithmetic_function/lib/arithmetic.rb
+++ Arithmetic_function/lib/arithmetic.rb
@@ -1,5 +1,9 @@
 def arithmetic(number_1, number_2, operator)
   if operator == 'add'
     number_1 + number_2
+  elsif operator == 'subtract'
+    number_1 - number_2
+  elsif operator == 'multiply'
+    number_1 * number_2
   end
 end
--- Arithmetic_function/spec/arithmetic_spec.rb
+++ Arithmetic_function/spec/arithmetic_spec.rb
@@ -4,4 +4,10 @@ describe '#arithmetic' do
   it 'adds 2 given numbers with with add operator' do
     expect(arithmetic(5,2, "add")).to eq 7
   end
+  it 'subtracts numbers with subtract operator' do
+    expect(arithmetic(5,2,"subtract")).to eq 3
+  end
+  it 'multiplies numbers with multiply operator' do
+    expect(arithmetic(5,2,"multiply")).to eq 10
+  end
 end
