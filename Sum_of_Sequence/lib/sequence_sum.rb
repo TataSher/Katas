06bd1940df6f6c21bce3fe20f7972da3ab8775cd
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-05 16:18:54 +0100

    refactored the solution;

--- Sum_of_Sequence/lib/sequence_sum.rb
+++ Sum_of_Sequence/lib/sequence_sum.rb
@@ -1,10 +1,3 @@
 def sequence_sum(start_value, end_value, step_value)
-  array = (start_value..end_value).to_a
-  @new_array = [start_value]
-  array.map do |value|
-    if @new_array.last < end_value
-      @new_array << @new_array.last + step_value
-    end
-  end
-  start_value > end_value ? 0 : @new_array.sum
+  start_value > end_value ? 0 : (start_value..end_value).step(step_value).to_a.sum
 end
