0348e3fc13600f930391134be842cf8b65e2666d
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-09 14:13:22 +0100

    method moves given consonant forward 9 places passes z and starts at a

--- Vowels_back/lib/vowel_back.rb
+++ Vowels_back/lib/vowel_back.rb
@@ -1,4 +1,8 @@
 def vowel_back(str)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
-    alphabet.include?(str) ? str = alphabet[alphabet.index(str) + 9] : str
+    if alphabet[0...-9].include?(str)
+      str = alphabet[alphabet.index(str) + 9]
+    elsif alphabet[17..25] 
+      str = alphabet[alphabet.index(str) - 17]
+    end
 end
--- Vowels_back/spec/vowel_back_spec.rb
+++ Vowels_back/spec/vowel_back_spec.rb
@@ -4,4 +4,7 @@ describe '#vowel_back' do
   it 'Moves consonants forward 9 places through the alphabet' do
     expect(vowel_back('b')).to eq('k')
   end
+  it "Moves consonants forward 9 places through the alphabet passing 'z', starting again at 'a'" do
+    expect(vowel_back('w')).to eq('f')
+  end
 end
