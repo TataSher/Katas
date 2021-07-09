6bf778e5503736f21383c0fb65f81af24652fd9c
Natalia_Sherchenkova <tataterer@gmail.com>
2021-07-09 18:26:53 +0100

    error fixed

--- Vowels_back/lib/vowel_back.rb
+++ Vowels_back/lib/vowel_back.rb
@@ -12,7 +12,7 @@ def vowel_back(str)
       letter = alphabet[alphabet.index(letter) - 17]
     elsif alphabet[4..25].include?(letter) && vowels.include?(letter)
       letter = alphabet[alphabet.index(letter) - 5]
-    elsif alphabet[0..3].include?(str) && vowels.include?(letter)
+    elsif alphabet[0..3].include?(letter) && vowels.include?(letter)
       letter = alphabet[21 + alphabet.index(letter)]
     elsif exception1.include?(letter)
       letter = alphabet[alphabet.index(letter) - 1]
