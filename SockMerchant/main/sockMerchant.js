05c3a739d639d85ac15336c374080e2147e7a99d
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-12 17:46:32 +0100

    Sorts out large pairs of socks

--- SockMerchant/main/sockMerchant.js
+++ SockMerchant/main/sockMerchant.js
@@ -1,5 +1,6 @@
 function sockMerchant(n, ar) {
   let pairCount = 0
+  ar.sort()
   for (let i = 0; i < n; i ++) {
 
     if (ar[i] === ar[i+1]) {
--- SockMerchant/test/sockMerchant.test.js
+++ SockMerchant/test/sockMerchant.test.js
@@ -24,3 +24,10 @@ test('when given 4 socks with 2 pairs returns 2', () => {
   expect(sockMerchant(3, [1, 1, 2, 2])).toBe(2)
 })
 
+test('when given 4 socks with a non consecutive pair returns 1', () => {
+  expect(sockMerchant(3, [1, 2, 3, 1])).toBe(1)
+})
+
+test('when given random sock amounts with colours returns pair count', () => {
+  expect(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20])).toBe(3)
+})
