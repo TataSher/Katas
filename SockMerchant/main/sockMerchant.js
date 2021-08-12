21604c7f34d7cee1953fd892936580793a227e05
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-12 17:38:18 +0100

    Sees 2 pairs of socks and counts them

--- SockMerchant/main/sockMerchant.js
+++ SockMerchant/main/sockMerchant.js
@@ -1,8 +1,10 @@
 function sockMerchant(n, ar) {
   let pairCount = 0
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i ++) {
+
     if (ar[i] === ar[i+1]) {
-      return Paircount = 1
+      pairCount++
+      i++
     }
   }
   return pairCount
--- SockMerchant/test/sockMerchant.test.js
+++ SockMerchant/test/sockMerchant.test.js
@@ -12,6 +12,15 @@ test('when given 2 different socks returns 0', () => {
   expect(sockMerchant(2, [1, 2])).toBe(0)
 })
 
-test('when given 3 socks  with one pair return 1', () => {
+test('when given 3 socks with one pair at the end returns 1', () => {
   expect(sockMerchant(3, [1, 2, 2])).toBe(1)
 })
+
+test('when given 3 socks with the same color returns 1', () => {
+  expect(sockMerchant(3, [1, 1, 1])).toBe(1)
+})
+
+test('when given 4 socks with 2 pairs returns 2', () => {
+  expect(sockMerchant(3, [1, 1, 2, 2])).toBe(2)
+})
+
