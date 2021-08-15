3123da7fec9f99322b462c480d0568f8860eafd4
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-15 19:22:53 +0100

    Jumping on Clouds jumps through the clouds as expected

--- Jumping_Clouds/main/jumpingOnClouds.js
+++ Jumping_Clouds/main/jumpingOnClouds.js
@@ -1,7 +1,11 @@
 function jumpingOnClouds(c) {
   let jumpCount = 0
   for (let i = 1; i < c.length; i++) {
-    c[i] === 1 || c[i] === 0 && c[i-1] === 0 && c[i+1] === 0 && c[i-2] !== 0 ? jumpCount : jumpCount++
+
+    c[i] === 1 ? jumpCount : jumpCount++
+     if (c[i] === 0 && c[i-1] === 0 && c[i+1] === 0){
+       i++
+    }
   }
   return jumpCount
 }
--- Jumping_Clouds/test/jumpingOnClouds.test.js
+++ Jumping_Clouds/test/jumpingOnClouds.test.js
@@ -12,6 +12,22 @@ test('with 2 cumulus clouds and 1 thunderhead in the middle returns 1', () => {
   expect(jumpingOnClouds([0,0,0])).toBe(1);
 });
 
-test('with 2 cumulus clouds and 1 thunderhead in the middle returns 1', () => {
+test('with 3 cumulus clouds and 1 thunderhead returns 2', () => {
+  expect(jumpingOnClouds([0, 1, 0, 0])).toBe(2);
+})
+
+test('with 6 cumulus clouds and 1 thunderhead in the middle returns 3', () => {
   expect(jumpingOnClouds([0, 0, 0, 0, 1, 0])).toBe(3);
+});
+
+test('with 3 cumulus clouds and 1 thunderhead returns 2', () => {
+  expect(jumpingOnClouds([0, 1, 0, 0])).toBe(2);
+});
+
+test('with 6 cumulus clouds returns 2', () => {
+  expect(jumpingOnClouds([0, 0, 0, 0, 0, 0])).toBe(3);
+});
+
+test('with a random combo return 28',() => {
+  expect(jumpingOnClouds([0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0])).toBe(28)
 });
