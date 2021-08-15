b45f7c792eb4a3780914f86d3b80201209bcc9bd
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-15 16:14:10 +0100

    Counting Valleys can count valleys with various steps

--- Counting_Valleys/main/countingValleys.js
+++ Counting_Valleys/main/countingValleys.js
@@ -1,7 +1,15 @@
 function countingValleys(steps, path) {
-   if (path[0] = 'D') {
-     return 1
-   }
-
+  altitude = 0
+  valleyCount = 0
+  for (let i = 0; i < steps; i++){
+    console.log(path[i])
+    path[i] === 'D' ? altitude-- : altitude ++;
+    if (altitude === -1 && path[i] === 'D') {
+      valleyCount++
+      console.log(valleyCount)
+    }
+  }
+  return valleyCount;
 }
+
 module.exports = countingValleys;
--- Counting_Valleys/test/readline.test.js
+++ Counting_Valleys/test/readline.test.js
@@ -5,5 +5,13 @@ test('when given 2 steps with a valley returns 1', () => {
 });
 
 test('when given 2 steps with a mountaint returns 0', () => {
-  expect(countingValleys(2, 'UD')).toBe(1);
+  expect(countingValleys(2, 'UD')).toBe(0);
 });
+
+test('when given 4 steps with 2 valleys returns 2', () => {
+  expect(countingValleys(4, 'DUDU')).toBe(2);
+})
+
+test('when given 4 steps with 2 valleys returns 2', () => {
+  expect(countingValleys(8, 'UDDDUDUU')).toBe(1);
+})
