197eac62b33ca1154c22e177585e732a609a0a1e
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-15 17:53:52 +0100

    Tests for 6 clouds pass

--- Jumping_Clouds/main/jumpingOnClouds.js
+++ Jumping_Clouds/main/jumpingOnClouds.js
@@ -1,10 +1,9 @@
 function jumpingOnClouds(c) {
   let jumpCount = 0
   for (let i = 1; i < c.length; i++) {
-    c[i] === 1 || i > 1 && c[i] === 0 && c[i-1] === 0 ? jumpCount : jumpCount++
-    
+    c[i] === 1 || c[i] === 0 && c[i-1] === 0 && c[i+1] === 0 && c[i-2] !== 0 ? jumpCount : jumpCount++
   }
-  return jumpCount;
+  return jumpCount
 }
 
 module.exports = jumpingOnClouds;
