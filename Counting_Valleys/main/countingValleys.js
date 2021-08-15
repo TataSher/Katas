b037b92ec29916f15ba34119dc4bfbf0c5bc52c9
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-15 16:15:15 +0100

    Defined altitude and valleyCount

--- Counting_Valleys/main/countingValleys.js
+++ Counting_Valleys/main/countingValleys.js
@@ -1,12 +1,10 @@
 function countingValleys(steps, path) {
-  altitude = 0
-  valleyCount = 0
+  let altitude = 0
+  let valleyCount = 0
   for (let i = 0; i < steps; i++){
-    console.log(path[i])
     path[i] === 'D' ? altitude-- : altitude ++;
     if (altitude === -1 && path[i] === 'D') {
       valleyCount++
-      console.log(valleyCount)
     }
   }
   return valleyCount;
