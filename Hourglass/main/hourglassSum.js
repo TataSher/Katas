53a3b1734d59c548d3389856b04aaa22a4251b4f
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-16 15:08:05 +0100

    Defined all the variables

--- Hourglass/main/hourglassSum.js
+++ Hourglass/main/hourglassSum.js
@@ -1,12 +1,11 @@
 function hourglassSum(arr) {
-  hourGlassValues = []
-  for (a = 1; a < arr.length - 1; a++) {
-    for (i = 1; i < arr[a].length-1; i++) {
+  const hourGlassValues = []
+  for (let a = 1; a < arr.length - 1; a++) {
+    for (let i = 1; i < arr[a].length-1; i++) {
       let hourGlass = arr[a-1][i-1] + arr[a-1][i] + arr[a-1][i+1] + arr[a][i] + arr[a+1][i-1] + arr[a+1][i] + arr[a+1][i+1]
       hourGlassValues.push(hourGlass)
     }
   }
-  console.log(hourGlassValues)
    return Math.max(...hourGlassValues)
 }
 module.exports = hourglassSum;
