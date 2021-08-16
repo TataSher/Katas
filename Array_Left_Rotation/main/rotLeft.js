6a9d2320ac904440eb1c42642d98b755ea4a2869
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-16 15:50:20 +0100

    Three number array passes

--- Array_Left_Rotation/main/rotLeft.js
+++ Array_Left_Rotation/main/rotLeft.js
@@ -1,7 +1,14 @@
 function rotLeft(a, d) {
-  newArray = [ ...Array(a).keys() ].map( i => i+1);
-
-  return newArray.reverse().join(' ');
+  let newArray = [ ...Array(a).keys() ].map( i => i+1);
+  for (let i = 0; i < a; i++) {
+    if (i === 0) {
+      newArray[newArray.length] = newArray[i];
+      newArray.shift()
+      console.log(newArray)
+    }
+    // } else { newArray[i-1] = newArray[i]}
+  }
+  return newArray.join(' ');
 
 }
 module.exports = rotLeft;
--- Array_Left_Rotation/test/rotLeft.test.js
+++ Array_Left_Rotation/test/rotLeft.test.js
@@ -3,3 +3,7 @@ const rotLeft = require('../main/rotLeft');
 test('When given array length 2 and rotation value 1 returns "2 1"', () => {
   expect(rotLeft(2, 1)).toBe("2 1");
 });
+
+test('When given array length 3 and rotation value 1 returns "2 3 1"', () => {
+  expect(rotLeft(3, 1)).toBe("2 3 1");
+});
