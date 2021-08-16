89e5b6df44e71b3bd67ea92e9d1ca00e0343b419
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-16 17:39:42 +0100

    Function throws error if more than 2 bribes happns

--- New_Year_Chaos/main/minimumBribes.js
+++ New_Year_Chaos/main/minimumBribes.js
@@ -2,8 +2,14 @@ function minimumBribes(q) {
   const originalQueue = [...q].sort()
   let bribeCount = 0
   for (let i = 0; i < q.length-1; i++) {
-    if ( originalQueue[i] === q[i+2] || originalQueue[i]=== q[i+1]) { 
+    if (originalQueue[i] === q[i+2] || originalQueue[i]=== q[i+1]) { 
       bribeCount++
+      i++
+    } else if (originalQueue[i] === q[i]){
+      bribeCount
+    } else { if (i != q.length - 1 || i != q.length - 2) {
+      throw "Too Chaotic"
+      }
     }
   }
   return bribeCount;
--- New_Year_Chaos/test/minimumBribes.test.js
+++ New_Year_Chaos/test/minimumBribes.test.js
@@ -7,3 +7,11 @@ test('with and array of 2 returns 1', () => {
 test('with and array of 3 returns 1', () => {
   expect(minimumBribes([2, 3, 1])).toBe(1);
 });
+
+test('with and array of 4 returns "Too Chaotic"', () => {
+  expect(() => {minimumBribes([2, 3, 4, 1])}).toThrow("Too Chaotic");
+});
+
+test('Long array with 1 bribe returns 1', () => {
+  expect(minimumBribes([1, 2, 3, 5, 4, 6, 7])).toBe(1);
+});
