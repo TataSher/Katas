d5f0d28f84df07d04f77064ff2a4962229c35cf8
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-16 14:58:14 +0100

    Array checks hourglass values horizontally

--- Hourglass/main/hourglassSum.js
+++ Hourglass/main/hourglassSum.js
@@ -1,6 +1,10 @@
 function hourglassSum(arr) {
-   let hourGlass = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2]
-  
-   return hourGlass
+  hourGlassValues = []
+  for (i = 1; i < arr[0].length-1; i++) {
+    let hourGlass = arr[0][i-1] + arr[0][i] + arr[0][i+1] + arr[1][i] + arr[2][i-1] + arr[2][i] + arr[2][i+1]
+    hourGlassValues.push(hourGlass)
+  }
+  console.log(hourGlassValues)
+   return Math.max(...hourGlassValues)
 }
 module.exports = hourglassSum;
--- Hourglass/test/hourglassSum.test.js
+++ Hourglass/test/hourglassSum.test.js
@@ -3,3 +3,7 @@ const hourglassSum = require('../main/hourglassSum');
 test('with array of 1s returns 7', () => {
   expect(hourglassSum([[1,1,1,1,1,1], [1,1,1,1,1,1], [1,1,1,1,1,1], [1,1,1,1,1,1], [1,1,1,1,1,1], [1,1,1,1,1,1]])).toBe(7);
 });
+
+test('with array of 1s returns 7', () => {
+  expect(hourglassSum([[1,1,1,1,1,1], [1,1,1,2,1,1], [1,1,1,1,1,1], [1,1,1,1,1,1], [1,1,1,1,1,1], [1,1,1,1,1,1]])).toBe(8);
+});
