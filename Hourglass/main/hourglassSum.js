4a1342d1f1c6d53674e900f37872b265d0cab7c6
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-16 14:49:07 +0100

    Single hourglass result calculated

--- Hourglass/main/hourglassSum.js
+++ Hourglass/main/hourglassSum.js
@@ -1,4 +1,6 @@
 function hourglassSum(arr) {
-  return 7
+   let hourGlass = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2]
+  
+   return hourGlass
 }
 module.exports = hourglassSum;
