5d5aeaf531d8c1cc79f0d8e0da925d0002f4adde
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-17 16:40:03 +0100

    Comments added to the solution

--- New_Year_Chaos/main/minimumBribes.js
+++ New_Year_Chaos/main/minimumBribes.js
@@ -1,22 +1,21 @@
 function minimumBribes(q) {
   let bribeCount = 0
-  let chaotic = false
-  for (let i = q.length; i > 0; i--) {
-    if ((i + 1) - q[i] > 2) {
-      chaotic = true
+
+  for (let i = 0; i < q.length; i++) {
+    // if the position of i is less than 2 it can't check the 2 digits in front
+    // it establishes the start of the line where j elements will be compared
+    let maxMove = q[i] - 2 > 0 ? q[i] - 2 : 0
+    // if position is swapped more than 2 - it's too chaotic
+    if (q[i] - (i + 1) > 2) {
+      return 'Too chaotic'
+    }
+    // 
+    for (let j = maxMove; j < i; j++) {
+      if (q[j] > q[i]){
+        bribeCount++
+      }
     }
   }
-  for (let i = q.length; i > 0; i--) {
-    if (q[i] === (i+1)){
-      bribeCount
-    } else if ((i + 1) - q[i] === 2 || (i + 1) - q[i]  === 1 ) { 
-      bribeCount++
-    }
-  }
-  if (chaotic === true) {
-    return "Too Chaotic"
-  } else {
-    return bribeCount
-  }
+  return bribeCount
 }
 module.exports = minimumBribes;
--- New_Year_Chaos/test/minimumBribes.test.js
+++ New_Year_Chaos/test/minimumBribes.test.js
@@ -5,11 +5,7 @@ test('with and array of 2 returns 1', () => {
 });
 
 test('with and array of 3 returns 1', () => {
-  expect(minimumBribes([2, 3, 1])).toBe(1);
-});
-
-test('with and array of 4 returns "Too Chaotic"', () => {
-  expect(minimumBribes([2, 3, 4, 1])).toBe("Too Chaotic");
+  expect(minimumBribes([2, 3, 1])).toBe(2);
 });
 
 test('Long array with 1 bribe returns 1', () => {
@@ -18,4 +14,12 @@ test('Long array with 1 bribe returns 1', () => {
 
 test('Long array with 3 bribe returns 3', () => {
   expect(minimumBribes([2, 1, 5, 3, 4])).toBe(3);
+});
+
+test('Long array with 3 bribe returns 3', () => {
+  expect(minimumBribes([5, 1, 2, 3, 7, 8, 6, 4])).toBe('Too chaotic');
+});
+
+test('Long array with 3 bribe returns 3', () => {
+  expect(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])).toBe(7);
 });
