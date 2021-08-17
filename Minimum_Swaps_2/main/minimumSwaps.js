acd5ec61b8817187ba950dfba7c6f76b3e96b54f
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-17 20:50:01 +0100

    3 digit array with 3 swaps is checked

--- Minimum_Swaps_2/main/minimumSwaps.js
+++ Minimum_Swaps_2/main/minimumSwaps.js
@@ -1,20 +1,13 @@
 function minimumSwaps(arr) {
   let swaps = 0
-  let isChecked = []
-  for (let i = 0; i < arr.length; i++) {
-    let values = {}
-    values['value'] = arr[i]
-    values['visited'] = false
-    isChecked.push(values)
-  }
-  for (let j = 0; j < isChecked.length; j++) {
-    if (isChecked[j]['visited'] === false) {
-      isChecked[j]['visited'] = true;
-
-      if (isChecked[j]['value'] !== j + 1 ){
-        let swapIndex = isChecked[j]['value']
-        isChecked[swapIndex-1]['visited'] = true
-        swaps++
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] !== i+1) {
+      let temp = arr[i];
+      arr[i] = arr[temp-1];
+      arr[temp-1] = temp;
+      swaps++
+      if (arr[i] !== i+1) {
+        i--
       }
     }
   }
