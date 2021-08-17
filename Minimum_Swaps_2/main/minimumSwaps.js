ef8e633787d8859daf21c94402b9a5c07c63885f
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-17 20:25:02 +0100

    3 digit array returns 1

--- Minimum_Swaps_2/main/minimumSwaps.js
+++ Minimum_Swaps_2/main/minimumSwaps.js
@@ -1,8 +1,22 @@
 function minimumSwaps(arr) {
   let swaps = 0
+  let isChecked = []
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i+1])
-    swaps++
+    let values = {}
+    values['value'] = arr[i]
+    values['visited'] = false
+    isChecked.push(values)
+  }
+  for (let j = 0; j < isChecked.length; j++) {
+    if (isChecked[j]['visited'] === false) {
+      isChecked[j]['visited'] = true;
+
+      if (isChecked[j]['value'] !== j + 1 ){
+        let swapIndex = isChecked[j]['value']
+        isChecked[swapIndex-1]['visited'] = true
+        swaps++
+      }
+    }
   }
   return swaps
 }
--- Minimum_Swaps_2/test/minimumSwaps.test.js
+++ Minimum_Swaps_2/test/minimumSwaps.test.js
@@ -7,3 +7,11 @@ test('An array of two numbers swapped returns 1', () => {
 test('An array with no swaps returns 0', () => {
   expect(minimumSwaps([1, 2, 3])).toBe(0);
 });
+
+test('An array of 3 numbers with edge numbers swapped returns 1', () => {
+  expect(minimumSwaps([3, 2, 1])).toBe(1);
+});
+
+test('An array of 4 numbers and 3 swaps', () => {
+  expect(minimumSwaps([4, 3, 1, 2])).toBe(3);
+});
