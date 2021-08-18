d0f9ee98c0b84b652fd39ec5c874c556ab8775ab
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-18 19:12:31 +0100

    Test with [1,2] returns 3

--- Minimum_Positive/test/minimumPositive.test.js
+++ Minimum_Positive/test/minimumPositive.test.js
@@ -5,5 +5,9 @@ test('gets [1] and returns 2', () => {
 });
 
 test('gets [2] and returns 1', () => {
-  expect(minimumPositive([1])).toBe(2);
+  expect(minimumPositive([2])).toBe(1);
+});
+
+test('gets [1, 2] and returns 3', () => {
+  expect(minimumPositive([1, 2])).toBe(3);
 });
