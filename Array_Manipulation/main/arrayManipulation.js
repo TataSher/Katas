1066a8946539516a9ed0e6f188b2d223b1c8a88e
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-18 13:45:01 +0100

    Longer array with two operations passes

--- Array_Manipulation/main/arrayManipulation.js
+++ Array_Manipulation/main/arrayManipulation.js
@@ -1,11 +1,15 @@
 function arrayManipulation(n, queries) {
   let array = new Array(n).fill(0);
-  let startIndex = queries[0][0]
-  let endIndex = queries[0][1]
-  let value = queries[0][2]
 
-  for (let i = startIndex; i <= endIndex; i++) {
-    array[i] += value
+  for (let j = 0; j < queries.length; j++){
+    let startIndex = queries[j][0]
+    let endIndex = queries[j][1]
+    let value = queries[j][2]
+   
+
+    for (let i = startIndex; i <= endIndex; i++) {
+      array[i] += value
+    }
   }
 
   return Math.max(...array)
--- Array_Manipulation/test/arrayManipulation.test.js
+++ Array_Manipulation/test/arrayManipulation.test.js
@@ -3,3 +3,7 @@ const arrayManipulation = require('../main/arrayManipulation');
 test('Given array of 2 and 1 operation', () => {
   expect(arrayManipulation(2, [[1,1,2]])).toBe(2);
 });
+
+test('Given array of 3 and 2 operation2', () => {
+  expect(arrayManipulation(3, [[1,2,2],[2,2,2]])).toBe(4);
+});
